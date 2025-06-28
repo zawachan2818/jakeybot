66d66532a2639963b54e07376d013ab4eeadeef4
zawachan2818 <82649784+zawachan2818@users.noreply.github.com>
2025-06-28 21:25:09 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# キャッシュ削除（Renderに効く場合あり）
-rm -rf ~/.cache/pip
-
-# 強制アンインストール
-pip uninstall -y discord.py discord py-cord || true
-
-# 明示的に py-cord を入れ直す
+pip uninstall -y discord discord.py py-cord || true
 pip install py-cord==2.5.0
-
-# 他のライブラリをインストール
 pip install -r requirements.txt
