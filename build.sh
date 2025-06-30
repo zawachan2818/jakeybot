4723bd58392d8f58c5df0c668ef15d53e1869ad2
zawachan2818 <82649784+zawachan2818@users.noreply.github.com>
2025-06-30 16:33:56 +0900

    build.sh を更新

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# 安全のため競合をアンインストール
+# 念のためのクリーンアップ
 pip uninstall -y discord discord.py py-cord
 
-# 依存をインストール
+# 依存関係インストール
 pip install -r requirements.txt
+
+# 最後にもう一度 discord.py 系を削除して py-cord を明示的に入れる
+pip uninstall -y discord discord.py
+pip install py-cord==2.5.0
