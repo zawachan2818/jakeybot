5a2291def007e75172cd96d158d7792ef817ce89
zawachan2818 <82649784+zawachan2818@users.noreply.github.com>
2025-06-30 17:48:30 +0900

    message_actions.py を更新

--- cogs/ai/gemini/message_actions.py
+++ cogs/ai/gemini/message_actions.py
@@ -1,7 +1,7 @@
 from aimodels.gemini import Completions
 from core.ai.assistants import Assistants
 from discord.ext import commands
-from google.genai import types
+from google.generativeai import types
 from os import environ
 import discord
 import logging
